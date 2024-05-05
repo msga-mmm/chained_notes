a7eff471eac67d9dce91e58905b8e09782d1bd02
msga-mmm <michael.sga.mmm@gmail.com>
2024-05-05 01:53:42 -0500

    feat: add jwt authentication with auth0

--- chained_notes/interfaces/api/views/note_view.py
+++ chained_notes/interfaces/api/views/note_view.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from chained_notes.adapters.database.models import Note
 from chained_notes.interfaces.api.serializers import NoteSerializer
@@ -7,3 +8,4 @@ from chained_notes.interfaces.api.serializers import NoteSerializer
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
